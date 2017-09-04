ef7a780f4c4cbc1fe40dfcceb9c51e60f7eb0820
Kun-Chieh (Jason) Wang <kwang65@wisc.edu>
2017-09-04 10:58:00 +0800

    Adding test case for class Hand

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from bridge.models import Trick, Trump, Player, Card, CardSuit, CardRank
+from bridge.models import Trick, Trump, Player, Card, CardSuit, CardRank, Hand
 
 
 def test_trick():
@@ -19,3 +19,36 @@ def test_trick():
     t = trick.play_card(Card(CardSuit.SPADE, CardRank.THREE))
     assert t is Player.WEST
     assert trick.winner == Player.WEST
+
+def test_Hand():
+    hand = Hand(cards=[Card(CardSuit.SPADE, CardRank.ACE), 
+                       Card(CardSuit.HEART, CardRank.ACE), 
+                       Card(CardSuit.DIAMOND, CardRank.ACE),
+                       Card(CardSuit.SPADE, CardRank.KING)]);
+    
+    card = hand.play_card(4);
+    assert card is None;
+    
+    card = hand.play_card(2, CardSuit.SPADE);
+    assert card is None;
+    
+    card = hand.play_card(1, CardSuit.DIAMOND);
+    assert card is None;
+    
+    card = hand.play_card(2)
+    assert card == Card(CardSuit.HEART, CardRank.ACE);
+    card = hand.play_card(1, CardSuit.SPADE);
+    assert card == Card(CardSuit.SPADE, CardRank.KING);
+    card = hand.play_card(0, CardSuit.CLUB);
+    assert card == Card(CardSuit.SPADE, CardRank.ACE);
+    
+    assert len(hand.cards[CardSuit.SPADE]) == 0;
+    hand.reverse();
+    assert len(hand.cards[CardSuit.SPADE]) == 1;
+    hand.reverse();
+    assert len(hand.cards[CardSuit.SPADE]) == 2;
+    assert hand.cards[CardSuit.SPADE].index(0).rank == CardRank.ACE;
+    
+    assert len(hand.cards[CardSuit.HEART]) == 0;
+    hand.reverse();
+    assert len(hand.cards[CardSuit.HEART]) == 1;
